3525b859094a80f179562bccf4dc8a60a3174eec
SuperPox <calebkohl@gmail.com>
2021-01-15 17:02:39 -0700

    commit tree 558a19422af69072b4e2532079939fb748644639

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,53 @@
+#squares each element in num array, returns new squared array, use .each, .collect, .inject
+require 'pry'
 def square_array(array)
-  # your code here
+  square_array = []
+  array.each do |number|
+    puts squarednumber = (number * number)
+    square_array.push(squarednumber)     
+  end
+  return square_array
 end
+
+
+
+
+=begin
+
+def square_array(array)
+  numbers = [1,2,3]
+  square_array = []
+  counter = 0
+  numbers.each do |squarednumber|
+    puts squarednumber = (squarednumber * squarednumber)
+    square_array.unshift(squarednumber)
+    counter += 1    
+  end
+  return square_array
+end
+
+def square_array(array)
+  numbers = [1,2,3]
+  counter = 0
+  numbers.each do |squarednumber|
+    puts squarednumber = (squarednumber * squarednumber)
+    counter += 1    
+  end
+end
+
+
+
+
+def square_array(array)
+  numbers = [1,2,3]
+  counter = 0
+  numbers.each do |squarednumber|
+    puts squarednumber * squarednumber = square
+    square_array = Array.new
+    square_array.unshift(square)
+    counter += 1    
+  end
+  return square_array[]
+end
+
+=end
